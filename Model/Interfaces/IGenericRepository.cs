b8f14ed2e2d4c86fc29f30c6bf5cb66bd0fa3bd4
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-09 14:17:28 +0800

    movie interface and repo setup

---
+++ Model/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMoviesWPF.Model.Interfaces
+{
+    internal interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+    }
+}
---
+++ ViewModel/MovieRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMoviesWPF.Model;
+using TheMoviesWPF.Model.Interfaces;
+
+namespace TheMoviesWPF.ViewModel
+{
+    internal class MovieRepository : IMovieRepository
+    {
+        public MovieRepository()
+        {
+            
+        }
+        public void Add(Movie entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
