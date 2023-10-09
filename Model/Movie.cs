d154ebe30c2fbd0da5f20a1c86cb1c92743f0d7b
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-09 14:04:49 +0800

    Add project files.

---
+++ Model/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMoviesWPF.Model
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public string Genre { get; set; }
+        public int Length { get; set; }
+
+    }
+}
