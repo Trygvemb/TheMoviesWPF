963fd29869f626369313093051ceda0edb832b45
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-09 15:40:39 +0800

    Movie Add method

--- Model/Movie.cs
+++ Model/Movie.cs
@@ -9,9 +9,16 @@ namespace TheMoviesWPF.Model
     internal class Movie
     {
         public int Id { get; set; }
-        public string Titel { get; set; }
+        public string Title { get; set; }
         public string Genre { get; set; }
         public int Length { get; set; }
 
+        public Movie(int id, string title, string genre, int length)
+        {
+            Id = id;
+            Title = title;
+            Genre = genre;
+            Length = length;
+        }
     }
 }
--- ViewModel/MovieRepository.cs
+++ ViewModel/MovieRepository.cs
@@ -9,22 +9,38 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Configuration;
+using System.Runtime.ConstrainedExecution;
 
 namespace TheMoviesWPF.ViewModel
 {
     internal class MovieRepository : IMovieRepository
     {
+        private readonly string ConnectionString;
+
+        public ICollection<Movie> movies;
         public MovieRepository()
         {
             IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-            string? ConnectionString = config.GetConnectionString("MyDBConnection");
-
+            ConnectionString = config.GetConnectionString("MyDBConnection");
 
         }
         public void Add(Movie entity)
         {
-            throw new NotImplementedException();
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Movie(Title, Genre, Lenth)" +
+                                                "VALUES(@Title, @Genre, @Lenth)" +
+                                                "SELECT @@IDENTITY", con);
+                cmd.Parameters.Add("@Titel", SqlDbType.NVarChar).Value = entity.Title;
+                cmd.Parameters.Add("@Genre", SqlDbType.NVarChar).Value = entity.Genre;
+                cmd.Parameters.Add("@Length", SqlDbType.Int).Value = entity.Length;
+                entity.Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                movies.Add(entity);
+            }
         }
 
         public IEnumerable<Movie> GetAll()
