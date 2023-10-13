6d349fd1964c9d573b52618ca407ef96e1bdb9ac
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-13 12:25:42 +0800

    Cinema

---
+++ Model/Cinema.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMoviesWPF.Model
+{
+    internal class Cinema
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Theater { get; set; }
+
+        public Cinema(string name, string theater)
+        {
+            Id = 0;
+            Name = name;
+            Theater = theater;
+        }
+    }
+}
