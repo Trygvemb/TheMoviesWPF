a6c650d95325208b3f3533475fd99f4f8926dd5d
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-13 12:41:21 +0800

    interface and repository for cinema and movieprogram

---
+++ ViewModel/CinemaRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMoviesWPF.Model;
+using TheMoviesWPF.Model.Interfaces;
+
+namespace TheMoviesWPF.ViewModel
+{
+    internal class CinemaRepository : ICinemaRepository
+    {
+        private readonly string ConnectionString;
+
+        public ObservableCollection<Cinema> cinemas = new ObservableCollection<Cinema>();
+        public CinemaRepository()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            ConnectionString = config.GetConnectionString("MyDBConnection");
+
+        }
+        public void Add(Cinema entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cinema> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Cinema entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Cinema entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ViewModel/MovieProgramRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMoviesWPF.Model;
+using TheMoviesWPF.Model.Interfaces;
+
+namespace TheMoviesWPF.ViewModel
+{
+    internal class MovieProgramRepository : IMovieProgramRepository
+    {
+        private readonly string ConnectionString;
+
+        public ObservableCollection<MovieProgram> moviePrograms = new ObservableCollection<MovieProgram>();
+        public MovieProgramRepository()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            ConnectionString = config.GetConnectionString("MyDBConnection");
+
+        }
+        public void Add(MovieProgram entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MovieProgram> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(MovieProgram entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(MovieProgram entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
