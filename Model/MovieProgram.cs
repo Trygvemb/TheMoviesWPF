23e8f314b96eef4f625252fe900d01ae9c0f3b6e
Trygvemb <126854646+Trygvemb@users.noreply.github.com>
2023-10-13 12:32:17 +0800

    MovieProgram

---
+++ Model/MovieProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMoviesWPF.Model
+{
+    internal class MovieProgram
+    {
+        public int Id { get; set; }
+        public string MovieDirector { get; set; }
+        public Movie Movie { get; }
+        public DateTime ShowTimes { get; set; }
+        public DateTime PremierDate { get; set; }
+        public Cinema Cinema { get; }
+
+        public MovieProgram(Movie movie, DateTime showTimes, string movieDirector, DateTime premierDate, Cinema cinema)
+        {
+            Id = 0;
+            Movie = movie;
+            ShowTimes = showTimes;
+            MovieDirector = movieDirector;
+            PremierDate = premierDate;
+            Cinema = cinema;
+        }
+    }
+}
